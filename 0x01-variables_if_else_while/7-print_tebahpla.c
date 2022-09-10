0810ab1ea098685d882bfde1a691516b1aad1fed
GNAMIENWA <ahuievrard@yahoo.fr>
2022-09-09 22:40:37 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,43 +1,79 @@
+Task 7.txt
+
+Qui a accès
+
+
+
+Propriétés système
+
+Type
+
+Texte
+
+Taille
+
+325 octets
+
+Espace de stockage utilisé
+
+325 octets
+
+Emplacement
+
+Variables if else while
+
+Propriétaire
+
+Antony Bahati
+
+Date de modification
+
+20:45 par Antony Bahati
+
+Ouvert
+
+05:37 par moi
+
+Date de création
+
+20:46
+
+Pas de description
+
+Téléchargement autorisé pour les lecteurs
+
 #include <stdio.h>
-
-#include <stdlib.h>
-
-#include <time.h>
-
-
 
 /**
 
- * main - No entry point
+ * main - print if the number is postive, zero, or negative
 
  *
 
- * Return: Always 0 (Success!)
+ * Description: using the main function
+
+ * this program prints "Programming is positive, zero, or negative
+
+ * Return: 0
 
  */
-
-
 
 int main(void)
 
 {
 
-	int num;
+char c;
 
+for (c = 'z'; c >= 'a'; c--)
 
+{
 
-	for (num = 0; num <= 9; num++)
+putchar(c);
 
-	{
+}
 
-		putchar(num + '0');
+putchar('\n');
 
-	}
-
-	putchar('\n');
-
-
-
-	return (0);
+return (0);
 
 }
