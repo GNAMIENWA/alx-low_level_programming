8632aa1bd207934f97dfeb0c61bfcf3488839752
GNAMIENWA <ahuievrard@yahoo.fr>
2022-09-26 23:48:31 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-		while (i < n)
-		{
-			*(s + i) = b;
-			 i++;
-		}
-		return (s);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		 i++;
+	}
+	return (s);
 }
